cd2b6e8500112e6153e6d4b97d6cff07a05ed19b
dongha-byun <byunsw4@naver.com>
2022-10-03 16:38:53 +0900

    Chore: Application Main 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,17 +1,10 @@
 package baseball;
 
-import baseball.domain.Computer;
-import baseball.domain.Player;
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
+import baseball.app.Baseball;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
-        String numberStr = Console.readLine();
-
-        Player player = new Player(numberStr);
-        Computer computer = new Computer();
+        Baseball.start();
     }
 }
