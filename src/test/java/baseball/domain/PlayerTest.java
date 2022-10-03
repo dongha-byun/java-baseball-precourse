de0d8695feaf55e18c089efb8006b2d8ad1b7268
dongha-byun <byunsw4@naver.com>
2022-10-03 16:44:47 +0900

    Test: 사용자 도메인 기능 테스트코드 추가
    입력 값 validation 및 입력 오류 테스트 추가

--- src/test/java/baseball/domain/PlayerTest.java
+++ src/test/java/baseball/domain/PlayerTest.java
@@ -11,7 +11,9 @@ class PlayerTest {
     @Test
     void 객체생성_테스트(){
         String numbers = "123";
-        Player player = new Player(numbers);
+        Player player = new Player();
+
+        player.inputNumbers(numbers);
 
         assertThat(player).isNotNull();
         assertThat(player.getNumbers()).isNotNull();
@@ -20,7 +22,9 @@ class PlayerTest {
     @Test
     void 사용자_입력숫자_생성(){
         String numbers = "425";
-        Player player = new Player(numbers);
+        Player player = new Player();
+
+        player.inputNumbers(numbers);
 
         assertThat(player.getNumbers().size()).isEqualTo(3);
         assertThat(player.getNumbers()).contains(4,2,5);
@@ -30,9 +34,22 @@ class PlayerTest {
     @ValueSource(strings = {"1234", "우테캠", "12동", "변99"})
     void 입력_오류_테스트(String input){
 
+        Player player = new Player();
         assertThatIllegalArgumentException().isThrownBy(
-                () -> new Player(input)
+                () -> player.inputNumbers(input)
         );
+    }
+
+    @Test
+    void 숫자_클리어_테스트(){
+
+        Player player = new Player();
+
+        player.inputNumbers("123");
+        assertThat(player.getNumbers().size()).isEqualTo(3);
+
+        player.clearNumbers();
+        assertThat(player.getNumbers().size()).isEqualTo(0);
     }
 
 }
